cb149b3b9329a94d5250b6f9ec2a061147c017f5
Kevin Lin <96133826+code-boom-boom@users.noreply.github.com>
2023-12-29 15:31:11 -0500

    initialize

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import * as THREE from 'three'
+import '../style.css'
+
+let scene, camera, renderer, fieldOfView, aspectRatio, near, far
+
+const container = document.querySelector('.world')
+
+const init = () => {
+    scene = new THREE.Scene()
+
+}
+
+init()
